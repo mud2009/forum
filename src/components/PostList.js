3396b1899504e4baa1be5c5faa424ef92a4c09e3
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-04-28 11:03:01 -0700

    Add sort functionality
    Co-authored-by: Will Greenberg <mud2009@users.noreply.github.com>

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,20 +5,22 @@ import PropTypes from "prop-types";
 function PostList(props) {
   return (
     <React.Fragment>
-      {Object.values(props.postList).map((post) => (
-        <Post
-          wholePost={post}
-          onHandleDownVote={props.onDownVote}
-          onHandleUpVote={props.onUpVote}
-          title={post.title}
-          author={post.author}
-          post={post.post}
-          upVote={post.upVote}
-          downVote={post.downVote}
-          id={post.id}
-          key={post.id}
-        />
-      ))}
+      {Object.values(props.postList)
+        .sort((a, b) => b.upVote + b.downVote - (a.upVote + a.downVote))
+        .map((post) => (
+          <Post
+            wholePost={post}
+            onHandleDownVote={props.onDownVote}
+            onHandleUpVote={props.onUpVote}
+            title={post.title}
+            author={post.author}
+            post={post.post}
+            upVote={post.upVote}
+            downVote={post.downVote}
+            id={post.id}
+            key={post.id}
+          />
+        ))}
     </React.Fragment>
   );
 }
